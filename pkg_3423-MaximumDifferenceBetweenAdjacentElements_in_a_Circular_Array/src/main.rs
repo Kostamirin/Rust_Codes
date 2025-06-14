3a24ddc7cca6b81098d19de3309a365c448a66ad
KerasinChick <base@gmain.com>
2025-06-14 19:39:58 +0300

    first_update

--- pkg_3423-MaximumDifferenceBetweenAdjacentElements_in_a_Circular_Array/src/main.rs
+++ pkg_3423-MaximumDifferenceBetweenAdjacentElements_in_a_Circular_Array/src/main.rs
@@ -6,11 +6,11 @@ impl Solution {
     pub fn max_adjacent_distance(nums: Vec<i32>) -> i32 {
         let n = nums.len();
         let mut result = (nums[0] - nums[n - 1]).abs();
-        
+
         for i in 0..n - 1 {
             result = result.max((nums[i] - nums[i + 1]).abs());
         }
-        
+
         result
     }
 }
